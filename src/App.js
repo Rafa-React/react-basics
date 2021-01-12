ba0a381c89238f475ceb85596cf0bf1cb1968745
mrafa <m.rafa11@outlook.com>
2021-01-12 14:29:36 +0530

    pushing project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className={'container-fluid'}>
+      <h1>Learning React with Mrityunjay</h1>
+      <h2>This is another header in h2</h2>
+    </div>
+  );
+}
+
+export default App;
